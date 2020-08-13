2657d8b3254ebd42e46d44515e045d3330302d1e
Juan Ricardo Diaz Suancha <juanricardo200@hotmail.com>
2020-08-12 22:33:50 -0500

    comentarios a la clase

--- Carvajal.Site/Models/UserAppViewModel.cs
+++ Carvajal.Site/Models/UserAppViewModel.cs
@@ -4,42 +4,75 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Carvajal.Site.Models
 {
+    /// <summary>
+    /// Usuario View Model
+    /// </summary>
     public class UserAppViewModel
     {
+        /// <summary>
+        /// Id del usuario
+        /// </summary>
         [Required]
         public int UserId { get; set; }
 
+        /// <summary>
+        /// Tipo de identificacion del usuario
+        /// </summary>
         [Required]
         public int TypeId { get; set; }
 
+        /// <summary>
+        /// Nombre de usuario 
+        /// </summary>
         [Required]
         [MaxLength(20, ErrorMessage = "No puede pasar de 20 caracteres")]
         [MinLength(3, ErrorMessage = "Como mínimo deben ser 3 caracteres")]
         public string UserName { get; set; }
 
+        /// <summary>
+        /// Apellido
+        /// </summary>
         [Required]
         [MaxLength(20, ErrorMessage = "No puede pasar de 20 caracteres")]
         [MinLength(3, ErrorMessage = "Como mínimo deben ser 3 caracteres")]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Numero de identificacion
+        /// </summary>
         [Required]
         [MaxLength(15, ErrorMessage = "No puede pasar de 15 caracteres")]
         [MinLength(4, ErrorMessage = "Como mínimo deben ser 4 caracteres")]
         public string IdentificationNumber { get; set; }
 
+        /// <summary>
+        /// Contraseña del usuario
+        /// </summary>
         [MaxLength(20, ErrorMessage = "No puede pasar de 20 caracteres")]
         [MinLength(6, ErrorMessage = "Como mínimo deben ser 6 caracteres")]
         public string UserPassword { get; set; }
 
+        /// <summary>
+        /// Confirmacion del password
+        /// </summary>
         public string ConfirmPassword { get; set; }
 
+        /// <summary>
+        /// Correo de usuario
+        /// </summary>
         [EmailAddress]
         [Required]
         public string UserEmail { get; set; }
 
+        /// <summary>
+        /// Descripcion del tipo de identificacion
+        /// </summary>
         public string TypeIdDescription { get; set; }
     }
 
+    /// <summary>
+    /// Clase Tools para el modelo UserApp y UserAppViewModel
+    /// </summary>
     public static class UserAppViewModelTools
     {
         /// <summary>
